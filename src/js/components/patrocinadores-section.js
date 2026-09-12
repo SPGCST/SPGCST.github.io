41aa8d5bfb7c9b177232936632d57fa6da2976af
diegopinheiro <dpmgeo@gmail.com>
2026-09-12 17:31:49 -0300

    Update patrocinadores-section.js

--- src/js/components/patrocinadores-section.js
+++ src/js/components/patrocinadores-section.js
@@ -5,7 +5,7 @@ class SponsorsSection extends HTMLElement {
         <div class="sponsors-grid">
           <div class="sponsor-card">
             <div class="sponsor-logo-wrapper">
-              <img src="src/assets/images/vale-topografia.png" alt="Vale Topografia" class="sponsor-logo" />
+              <img src="./src/assets/images/vale-topografia.png" alt="Vale Topografia" class="sponsor-logo" />
             </div>
             <div class="sponsor-meta">
               <span class="sponsor-name">Vale Topografia</span>
@@ -14,7 +14,7 @@ class SponsorsSection extends HTMLElement {
 
           <div class="sponsor-card">
             <div class="sponsor-logo-wrapper">
-              <img src="src/assets/images/geoone.jpg" alt="GeoOne" class="sponsor-logo" />
+              <img src="./src/assets/images/geoone.png" alt="GeoOne" class="sponsor-logo" />
             </div>
             <div class="sponsor-meta">
               <span class="sponsor-name">GeoOne</span>
